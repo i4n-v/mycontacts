18e75fd80befc622d9f21ca7b2500a91ccd19fcc
i4n-v <ianvinicioss@gmail.com>
2022-11-19 09:54:47 -0300

    feat: adding routes to create, list, find, update and delete categories

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 
 const ContactController = require('./app/controllers/ContactController');
+const CategorieController = require('./app/controllers/CategorieController');
 
 const router = Router();
 
@@ -13,5 +14,12 @@ router.get('/contacts/:id', ContactController.show);
 router.delete('/contacts/:id', ContactController.delete);
 router.post('/contacts', ContactController.store);
 router.put('/contacts/:id', ContactController.update);
+
+// categorie
+router.get('/categories', CategorieController.index);
+router.get('/categories/:id', CategorieController.show);
+router.delete('/categories/:id', CategorieController.delete);
+router.post('/categories', CategorieController.store);
+router.put('/categories/:id', CategorieController.update);
 
 module.exports = router;
