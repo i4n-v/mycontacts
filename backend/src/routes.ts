01be5958ba5da7f69c6eb9454f9b04da962f9a2a
ian.vinicius <ian.vinicius@tascomtecnologia.com.br>
2024-09-23 22:27:57 -0300

    feat: setup typescript

---
+++ backend/src/routes.ts
@@ -0,0 +1,25 @@
+import { Router } from "express";
+
+const ContactController = require("./app/controllers/ContactController");
+const CategorieController = require("./app/controllers/CategorieController");
+
+const router = Router();
+
+// root
+router.get("/", (request, response) => response.send("Hello world"));
+
+// contact
+router.get("/contacts", ContactController.index);
+router.get("/contacts/:id", ContactController.show);
+router.delete("/contacts/:id", ContactController.delete);
+router.post("/contacts", ContactController.store);
+router.put("/contacts/:id", ContactController.update);
+
+// categorie
+router.get("/categories", CategorieController.index);
+router.get("/categories/:id", CategorieController.show);
+router.delete("/categories/:id", CategorieController.delete);
+router.post("/categories", CategorieController.store);
+router.put("/categories/:id", CategorieController.update);
+
+export default router;
