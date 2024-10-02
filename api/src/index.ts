bf0875a69e602510821d19ab1861789e23de848c
ian.vinicius <ianvinicioss@gmail.com>
2024-10-01 22:11:30 -0300

    chore: modify some rules

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,18 +1,14 @@
-import "express-async-errors";
-import express, { NextFunction, Request, Response } from "express";
-import routes from "./routes";
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    console.log(error);
-    response.sendStatus(500);
-  }
-);
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  console.log(error);
+  response.sendStatus(500);
+});
 
-app.listen(3000, () =>
-  console.log("🔥 Server started at http://localhost:3000")
-);
+app.listen(3000, () => console.log('🔥 Server started at http://localhost:3000'));
