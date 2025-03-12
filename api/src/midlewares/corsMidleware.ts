7db751b7d1e4a943e77338f2c1d4db87a69a82d0
i4n-v <ianvinicioss@gmail.com>
2025-03-12 14:36:56 -0300

    feat: adjusting cors midleware

--- api/src/midlewares/corsMidleware.ts
+++ api/src/midlewares/corsMidleware.ts
@@ -4,6 +4,9 @@ import { NextFunction, Request, Response } from 'express';
 
 function corsMidleWare(request: Request, response: Response, next: NextFunction) {
   response.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN!);
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  response.setHeader('Access-Control-Max-Age', '10');
   next();
 }
 
