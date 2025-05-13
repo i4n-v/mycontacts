89f96abe25b1a2ab97445b99fd93a3deb8a70121
i4n-v <ianvinicioss@gmail.com>
2025-05-12 21:04:05 -0300

    feat: adding multiple origins in cors midleware

--- api/src/midlewares/corsMidleware.ts
+++ api/src/midlewares/corsMidleware.ts
@@ -3,10 +3,17 @@ import 'dotenv/config';
 import { NextFunction, Request, Response } from 'express';
 
 function corsMidleWare(request: Request, response: Response, next: NextFunction) {
-  response.setHeader('Access-Control-Allow-Origin', process.env.CORS_ORIGIN!);
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
-  response.setHeader('Access-Control-Max-Age', '10');
+  const origin = request.header('Origin')!;
+  const allowedOrigins = process.env.ALLOWED_ORIGINS!.split(',');
+  const isAllowed = allowedOrigins.includes(origin);
+
+  if (isAllowed) {
+    response.setHeader('Access-Control-Allow-Origin', origin);
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '10');
+  }
+
   next();
 }
 
