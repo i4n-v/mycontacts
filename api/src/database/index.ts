853994573ab7e83cf28f661538a7f0f69b88ae6d
i4n-v <ianvinicioss@gmail.com>
2025-09-04 11:40:45 -0300

    feat: adjuting ssl connection to db

--- api/src/database/index.ts
+++ api/src/database/index.ts
@@ -7,6 +7,7 @@ const client = new Client({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
+  ssl: process.env.DB_SSL === 'true',
 });
 
 client.connect();
